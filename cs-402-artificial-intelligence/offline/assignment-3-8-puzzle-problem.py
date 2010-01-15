e3cd8910e323bd9ce540895944e83e8446b2c8cf
Abu Zaher <zaher14@gmail.com>
2010-01-15 16:44:37 +0600

    modified:   assignment-3-8-puzzle-problem.py, comments

--- cs-402-artificial-intelligence/offline/assignment-3-8-puzzle-problem.py
+++ cs-402-artificial-intelligence/offline/assignment-3-8-puzzle-problem.py
@@ -131,7 +131,9 @@ if __name__ == '__main__':
 # 	endnode = [7, 4, 6, 5, 2, 0, 8, 3, 1]
 	endnode = [7, 4, 6, 8, 5, 3, 0, 1, 2]
 # 	endnode = [7, 4, 6, 5, 2, 1, 0, 8, 3]
+	# takes a lot of time
 # 	endnode = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+	# this node is unsolvable
 # 	endnode = [1, 2, 4, 5, 0, 6, 8, 3, 7]
 
 	# 15 puzzle
